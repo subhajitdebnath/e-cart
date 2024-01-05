4884a26523759a1844a00b7aef1d338860c1d346
SubhajitD <subhajitdebnath.999@gmail.com>
2024-01-05 08:31:57 +0530

    login changes

--- src/app/pre-auth/login/login.component.ts
+++ src/app/pre-auth/login/login.component.ts
@@ -7,55 +7,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  email: string=''
-  
-  loginForm!: FormGroup;
-
-
-  constructor(private router: Router,private loginService: LoginService) { }
-
-  ngOnInit(): void{
-    this.loginForm= new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
-
-    this.loginForm.get('email')?.statusChanges.subscribe(data => { 
-      console.log(data);
-    })
-    this.loginForm.get('password')?.statusChanges.subscribe(data => { 
-      console.log(data);
-    })
-  }
-  addCustomerForm(formvalue:NgForm){
-
-  }
-
-  // this.loginForm.get('email')?.statusChanges.subscribe(data => { 
-  //   console.log(data);
-  // })
-  // this.loginForm.stateChanges.subscribe(data=>{
-  //   console.log(data);
-  // }
-  trackEmail(){
-    this.loginForm.get('email')?.valueChanges.subscribe(data=>{
-      console.log(data);
-    })
-  }
-
-
- 
-
-  onLoginSuccess(){
-    console.log(this.loginForm.value)
-    if(this.loginForm.valid){
-      this.router.navigate(['/']);
-      this.loginService.login();
-    }
-    
-  }
+export class LoginComponent {
 
   loginForm!: FormGroup;
 
