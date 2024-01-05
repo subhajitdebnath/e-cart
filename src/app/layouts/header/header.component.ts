cc9888dac80f8b2427de5c760d1730c87fea021f
praveendevarala524 <devaralap@gmail.com>
2024-01-05 08:21:07 +0530

    Practice Changes

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/pre-auth/login/login.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  isLoggedIn = false;
+
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    // this.loginService.isLoggedIn$.subscribe((isLoggedIn) => {
+    //   this.isLoggedIn = isLoggedIn;
+    //   // this.username = this.login
+    // });
+  }
+
+  logout(): void {
+    this.loginService.logout();
+  }
+  search(){
+    
+
+  }
 
 }
--- src/app/pre-auth/pre-auth.module.ts
+++ src/app/pre-auth/pre-auth.module.ts
@@ -5,6 +5,9 @@ import { PreAuthRoutingModule } from './pre-auth-routing.module';
 import { PreAuthComponent } from './pre-auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -15,7 +18,9 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    PreAuthRoutingModule
+    PreAuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class PreAuthModule { }
